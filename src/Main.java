056eea3e00aef82fa08b8dbaba2b9d688891240c
Jan Niclas Ruppenthal <jan.rupp06@web.de>
2022-11-11 15:07:17 +0100

    Added functionality in Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,32 @@
+import java.io.File;
+import java.util.Random;
+
 public class Main
 {
+    public static void main(String[] args)
+    {
+        String pathToDirectory = args[0];
+        File directory = new File(pathToDirectory);
 
+        if (!directory.isDirectory())
+        {
+            throw new RuntimeException(pathToDirectory + " is not a directory!");
+        }
+
+        directory = pickUpFile(directory);
+
+        System.out.println("Pick up file: " + directory.getName() + " in " + directory.getPath());
+    }
+
+    private static File pickUpFile(File directory)
+    {
+        int numberOfFiles = 0;
+        int randomNumber = 0;
+        while (directory.isDirectory() && (numberOfFiles = directory.listFiles().length) > 0)
+        {
+            randomNumber = new Random().nextInt(numberOfFiles);
+            directory = directory.listFiles()[randomNumber];
+        }
+        return directory;
+    }
 }
